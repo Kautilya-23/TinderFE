b5ab5fb84896628fcfd4235aadd60ccd07cf0f76
Kautilya-23 <kautilyasathwara@gmail.com>
2025-08-15 13:00:21 +0530

    Setup Redux store

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./Navbar";
+import NavBar from "./NavBar";
 import Footer from "./Footer";
+import Login from "./Login";
 
 const Body = () => {
     return (
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,6 @@
 const Profile = () => {
     return(
-        <div>Profile Page</div>
+        <div>Profile</div>
     )
 }
 
